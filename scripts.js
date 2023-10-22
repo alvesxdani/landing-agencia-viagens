3e56e4ebdc8f122ba30f769bd35a56a2a51f718e
alvesxdani <dani.alvesm22@gmail.com>
2023-10-22 20:05:33 -0300

    :sparkles: finalização do projeto

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const nome_contato = document.getElementById('nome-contato')
+const tel_contato = document.getElementById('tel-contato')
+const form = document.getElementById('form-contato')
+const lista_contatos = document.getElementById('lista-contatos')
+let contatos = []
+
+function listarContatos(nome, tel) {
+  lista_contatos.innerHTML += `
+    <li><strong>${nome}</strong>: ${tel}</li>
+    `
+  console.log(nome, tel)
+}
+
+function addContato() {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (nome_contato.value !== '' && tel_contato.value !== '') {
+      listarContatos(nome_contato.value, tel_contato.value)
+    }
+
+    nome_contato.value = ''
+    tel_contato.value = ''
+  })
+}
+
+addContato()
