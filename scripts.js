36d80a67573bdcbdef0c5e2b1d5731da1ddc1abf
alvesxdani <dani.alvesm22@gmail.com>
2023-11-05 21:57:45 -0300

    :sparkles: Add line-through on list item

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-$('.forms').submit(function(e) {
-  e.preventDefault();
-  let valor = $('#nome-tarefa').val()
-  $(`
-    <li>${valor}</li>
-  `).appendTo('#tarefas')
-})
+$(document).ready(function() {
+  $('.forms').submit(function(e) {
+    e.preventDefault();
+  
+    let valor = $('#nome-tarefa').val()
+  
+    $(`
+      <li>${valor}</li>
+    `).appendTo('#tarefas')
+  })
+
+  $("ul").on("click", "li", function() {
+    if (!$(this).hasClass("riscado")) {
+      $(this).addClass("riscado");
+    } else {
+      $(this).removeClass("riscado");
+    }
+  });
+});
+
